72c53d241c5fa318cedeb6d7a2ebe50f9f64d5a9
kdy <kkykdy4334@gmail.com>
2023-05-13 14:29:20 +0900

    feat: make connect wallets

--- src/components/pages/testPages/TestPage.jsx
+++ src/components/pages/testPages/TestPage.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+// import * as colors from '@styles/colors';
+import ConnectWallet from '@articles/ConnectWallet';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  margin: 1rem;
+`
+
+const WalletWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 15rem;
+`;
 
 const TestPage = () => {
   return (
-    <div>
-      <span>Hello World!</span>
-    </div>
+    <Container>
+      <WalletWrapper>
+        <ConnectWallet />
+      </WalletWrapper>
+    </Container>
   );
 };
 
