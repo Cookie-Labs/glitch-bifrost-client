ccb648fd9efb528b169325fc835e185ae025f1c6
kdy <kkykdy4334@gmail.com>
2023-05-20 04:54:00 +0900

    feat: make useChainRunner with no test

--- src/components/pages/testPages/ShoeboxOverviewPage.jsx
+++ src/components/pages/testPages/ShoeboxOverviewPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import useWeb3 from '@hooks/useWeb3';
 import useOracle from '@hooks/useOracle';
@@ -6,21 +6,54 @@ import useChainRunner from '@hooks/useChainRunner';
 
 const TestButton = styled.button`
   border: 1px solid white;
-  padding: 1rem;
-  font-size: 1.2rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: white;
+    transition: 0.3s;
+  }
 `;
 
 const ShoeboxOverviewPage = () => {
   const { web3, signMessage, isSigned } = useWeb3();
   const { BFC, BIFI, ETH, BNB, MATIC } = useOracle();
-  const {implementBridge} = useChainRunner();
+  const { client, approveBridge, implementBridge } = useChainRunner();
+  const [bridgeAmount, setBridgeAmount] = useState(0);
 
-  console.log(web3);
+  // console.log(web3);
+  // console.log(client);
+
+  async function doBridge() {
+    try {
+      await approveBridge(bridgeAmount);
+      await implementBridge(bridgeAmount);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const handleBridgeAmount = (e) => {
+    const newValue = e.target.value;
+    if (/^\d*$/.test(newValue)) {
+      setBridgeAmount(Number(newValue));
+    } else {
+      e.target.value = bridgeAmount;
+    }
+  };
 
   return (
     <>
-      <TestButton onClick={() => signMessage('HELLO')}>TEST SIGN</TestButton>
-      <TestButton onClick={() => implementBridge()}>TEST bridge mumbai to bifrost</TestButton>
+      <div>
+        <TestButton onClick={() => signMessage('HELLO')}>TEST SIGN</TestButton>
+      </div>
+      <div>
+        <input onChange={handleBridgeAmount} value={bridgeAmount} />
+        <TestButton onClick={doBridge}>
+          TEST bridge polygonMainnet to bifrostMainnet
+        </TestButton>
+      </div>
       <div>{isSigned}</div>
       <div>BFC: {BFC}USDT</div>
       <div>BIFI: {BIFI}USDT</div>
