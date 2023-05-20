cc219053613796141022fda480bc2571c4861f44
김동헌 <oueya1479@naver.com>
2023-05-20 19:59:27 +0900

    swap

--- src/components/pages/testPages/ShoeboxSwapPage.jsx
+++ src/components/pages/testPages/ShoeboxSwapPage.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import * as colors from '@styles/colors';
+import ShoeBoxNetworkModal from './ShoeBoxNetworkModal';
+import useSwap from '@hooks/useSwap';
 
 const Container = styled.div`
   width: 100%;
@@ -32,19 +34,47 @@ const TopTitleWrapper = styled.div`
   border-bottom: 1px solid ${colors.bgQuaternary};
 `;
 
+const FromAmount = styled.input`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  padding: 0px;
+  font-weight: 400;
+  font-size: 20px;
+  color: rgb(255, 255, 255);
+  background-color: transparent;
+  border: none;
+  outline: none;
+`;
+
 const TopTitle = styled.span`
   font-size: 1.7rem;
   font-weight: 900;
-`
+`;
+
+const SelectNetwork = styled.button`
+  text-align: left;
+  width: 100%;
+`;
 
 const ResetButton = styled.button`
   cursor: pointer;
   color: ${colors.bgWhite};
 
-  &:hover{
+  &:hover {
     color: ${colors.textPrimary};
   }
-`
+`;
+
+const SelectToken = styled.a``;
+
+const TokenWrapper = styled.div``;
+
+const InfoWrapper = styled.div``;
+
+const ExchangeRate = styled.div``;
+
+const CurrentBalance = styled.div``;
 
 const SwapWrapper = styled.div`
   width: 100%;
@@ -53,12 +83,29 @@ const SwapWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  padding: 2rem;
   background-color: ${({ isFrom }) =>
     isFrom ? colors.bgPrimary : colors.bgWhite};
   border-radius: 0 0 1rem 1rem;
 `;
 
 const ShoeboxSwapPage = () => {
+  const [network, setNetwork] = useState('Select Network');
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentBalance, setCurrentBalance] = useState('0');
+  const [exchange, setExchange] = useState('0');
+
+  const { allPairs } = useSwap();
+  console.log(allPairs);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <Container>
       <Wrapper>
@@ -66,8 +113,28 @@ const ShoeboxSwapPage = () => {
           <TopTitle>Shoe Box Cross Chain Swap</TopTitle>
           <ResetButton>Reset</ResetButton>
         </TopTitleWrapper>
-        <SwapWrapper isFrom={true}>Top</SwapWrapper>
-        <SwapWrapper isFrom={false}>Bottom</SwapWrapper>
+        <SwapWrapper isFrom={false}>
+          <SelectNetwork onClick={openModal}>{network}</SelectNetwork>
+          <TokenWrapper>
+            <FromAmount placeholder="From Amount"></FromAmount>
+            <SelectToken>LDO</SelectToken>
+          </TokenWrapper>
+          <InfoWrapper>
+            <ExchangeRate>$ {exchange}</ExchangeRate>
+            <CurrentBalance>Balance : {currentBalance}</CurrentBalance>
+          </InfoWrapper>
+        </SwapWrapper>
+        <SwapWrapper isFrom={true}>
+          <SelectNetwork onClick={openModal}>{network}</SelectNetwork>
+          <TokenWrapper>
+            <FromAmount placeholder="To Amount"></FromAmount>
+            <SelectToken>LDO</SelectToken>
+          </TokenWrapper>
+          <InfoWrapper>
+            <ExchangeRate>$ {exchange}</ExchangeRate>
+            <CurrentBalance>Balance : {currentBalance}</CurrentBalance>
+          </InfoWrapper>
+        </SwapWrapper>
       </Wrapper>
     </Container>
   );
--- src/hooks/useSwap.js
+++ src/hooks/useSwap.js
@@ -1,1 +1,85 @@
-export default function useSwap() {}
+import { useEffect, useState } from 'react';
+import Web3 from 'web3';
+import useWeb3 from './useWeb3';
+import { FACTORY_ABI, ERC20_ABI } from './useABI';
+
+const POLYGON_ROUTER = process.env.REACT_APP_SWAP_ROUTER_POLYGON_TEST_ADDR;
+const ERC20 = process.env.REACT_APP_SWAP_ERC20_POLYGON_TEST_ADDR;
+
+async function authenticate(_web3) {
+    if (Object.keys(_web3).length === 0 ||
+        await _web3.eth.getChainId() !== 80001) return false;
+    return true;
+}
+
+export default function useSwap() {
+    const [allPairs, setAllPairs] = useState(0);
+    const { web3 } = useWeb3();
+    let polygonRouter;
+
+    async function auth() {
+        if (await authenticate(web3)) {
+            polygonRouter = new web3.eth.Contract(
+                FACTORY_ABI,
+                POLYGON_ROUTER,
+            );
+        }
+    }
+
+    auth();
+
+    useEffect(() => {
+        async function approve() {
+            if (!await authenticate(web3)) return;
+
+            const ERC20Token = new web3.eth.Contract(
+                ERC20_ABI,
+                ERC20
+            );
+
+            ERC20Token.methods.approve(
+                POLYGON_ROUTER,
+                100,
+            )
+                .send({ from: process.env.REACT_APP_ADMIN_ADDR, value: 0 })
+                .catch(e => {
+                    if (e.code === 4001) {
+                        console.log("user rejected the transaction");
+                    }
+                })
+        }
+
+        // async function addLiquidity() {
+        //     if (Object.keys(web3).length === 0 || await web3.eth.getChainId() !== 80001) return;
+
+        //     const polygonRouter = new web3.eth.Contract(
+        //         POLYGON_ABI,
+        //         POLYGON_ROUTER,
+        //     );
+
+        //     const accounts = await window.ethereum.request({
+        //         method: 'eth_requestAccounts',
+        //     });
+
+        //     const length = await polygonRouter.methods
+        //         .addLiquidity(
+        //             "0x57714804960e11693B462934C4440C7DaD94515a",
+        //             "0x5B67676a984807a212b1c59eBFc9B3568a474F0a",
+        //             10,
+        //             10,
+        //             1,
+        //             1,
+        //             process.env.REACT_APP_ADMIN_ADDR,
+        //             1000000000000,
+        //         )
+        //         .send({ from: process.env.REACT_APP_ADMIN_ADDR, value: 0 });
+
+        //     setAllPairs(length);
+        // }
+
+        approve();
+
+    }, [web3]);
+
+    return { allPairs };
+}
